b18d536c8fd6dd11642b7fe72fe53574461fe9b7
Antony MONCLER <prof.antony@hotmail.fr>
2021-04-26 01:02:56 +0200

    new

---
+++ brouillon.py
@@ -0,0 +1,15 @@
+    class Passenger :
+
+        def __init__(self,numerox,numeroy,bagage):
+            self.numerox = numerox
+            self.numeroy = numeroy
+            self.bagage = bagage
+            print("bienvenue au passager", "place", numerox,numeroy,"avec nb bagage",bagage)
+
+    def liste_passager():
+    for i in range(7):
+        for j in range(30):
+            passenger = passenger(i,j,rd.randint(0,2))
+            place[i][j] = 1
+    print(place)
+    pass
--- workanto.py
+++ workanto.py
@@ -1,12 +1,15 @@
 import tkinter as tk 
+from timeit import default_timer
+import random as rd
 
 HEIGHT = 750
 WIDTH = 175
 largeur_case = WIDTH // 7
 hauteur_case = HEIGHT // 30
+place = [[0]*7 for i in range(30)]
+
 
 def avion():
-    canvas = tk.Canvas(racine, bg="red", height=HEIGHT, width=WIDTH)
     canvas.grid()
     for i in range(7):
         for j in range(30):
@@ -17,9 +20,27 @@ def avion():
             canvas.create_rectangle((i*largeur_case, j*hauteur_case),((i+1)*largeur_case, (j+1)*hauteur_case), fill=color)
 
 def passager():
-    pass
+    x0,y0 = 88,12
+    canvas.create_oval(x0,y0,x0+5,y0+5,width=1,fill="black")
+    
+
+
+def temps():
+    now = default_timer() - start
+    minutes, seconds = divmod(now, 60)
+    hours, minutes = divmod(minutes, 60)
+    str_time = "%d:%02d:%02d" % (hours, minutes, seconds)
+    canvas.itemconfigure(text_clock, text=str_time)
+    racine.after(1000, temps)
+
 
 
 racine = tk.Tk() # Création de la fenêtre racine
+canvas = tk.Canvas(racine, bg="red", height=HEIGHT, width=WIDTH)
 avion()
+passager()
+start = default_timer()
+text_clock = canvas.create_text(40, 20)
+temps()
+print(place)
 racine.mainloop() # Lancement de la boucle principale
