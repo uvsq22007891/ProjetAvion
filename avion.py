6f7e80ac395290c414d05ad2dbfa986527509665
Antony MONCLER <prof.antony@hotmail.fr>
2021-04-26 10:46:37 +0200

    new

--- avion.py
+++ avion.py
@@ -1,5 +1,22 @@
 import tkinter as tk
 
+HEIGHT = 750
+WIDTH = 175
+largeur_case = WIDTH // 7
+hauteur_case = HEIGHT // 30
+place = [[0]*7 for i in range(30)]
+
+
+def avion():
+    canvas.grid()
+    for i in range(7):
+        for j in range(30):
+            if i == 3:
+                 color = "red"
+            else:
+                color = "blue"
+            canvas.create_rectangle((i*largeur_case, j*hauteur_case),((i+1)*largeur_case, (j+1)*hauteur_case), fill=color)
+
 def mouv(event):
     pass
 def temps():
@@ -15,5 +32,7 @@ def random_bagage():
 
 
 racine = tk.Tk() # Création de la fenêtre racine
+canvas = tk.Canvas(racine, bg="red", height=HEIGHT, width=WIDTH)
+avion()
 
 racine.mainloop() # Lancement de la boucle principale
